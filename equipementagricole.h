866cfbc0cac422924b116971063f380b75eb13f7
rafed-99 <73440128+rafed-99@users.noreply.github.com>
2020-12-07 16:24:27 +0100

    Update equipementagricole.h

--- equipementagricole.h
+++ equipementagricole.h
@@ -7,9 +7,9 @@ char modele[30];
 char fabricateur[30];
 char marque[30];
 int fonctionnement;
-}eqpagri;
+}equipagri;
 
-void ajouter(eqpagri e);
-void modifier(eqpagri e);
-void supprimer(eqpagri e);
-eqpagri chercher(eqpagri e);
+void ajouter(equipagri e);
+void modifier(equipagri e);
+void supprimer(equipagri e);
+eqpagri chercher(equipagri e);
